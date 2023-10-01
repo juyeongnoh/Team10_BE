a79fbf49fcb4dbd5b7a1f0c1a19db443142caa09
김명지 <audwl03071@gmail.com>
2023-10-01 19:26:44 +0900

    feat: 리뷰와 키워드 다대다 매핑을 위한 Entity 구현

---
+++ src/main/java/bdbe/bdbd/rkeyword/reviewKeyword/ReviewKeywordJPARepository.java
@@ -0,0 +1,7 @@
+package bdbe.bdbd.rkeyword.reviewKeyword;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewKeywordJPARepository extends JpaRepository<ReviewKeyword, Long> {
+
+}
