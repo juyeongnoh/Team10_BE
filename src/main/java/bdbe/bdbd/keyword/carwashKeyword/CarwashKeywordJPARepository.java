6819684bec27e8aab00c7a4935972ea37e802438
김명지 <audwl03071@gmail.com>
2023-10-01 19:30:37 +0900

    feat: 세차장과 키워드 다대다 매핑을 위한 Entity 작성

---
+++ src/main/java/bdbe/bdbd/keyword/carwashKeyword/CarwashKeywordJPARepository.java
@@ -0,0 +1,7 @@
+package bdbe.bdbd.keyword.carwashKeyword;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarwashKeywordJPARepository extends JpaRepository<CarwashKeyword, Long> {
+
+}
