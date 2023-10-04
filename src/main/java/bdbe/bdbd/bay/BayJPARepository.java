73b794d3b0a8dbc4a3bf36ddcb8c4915203f2b83
김명지 <audwl03071@gmail.com>
2023-10-04 23:29:19 +0900

    feat: 예약 기능 테스트 코드 작성

--- src/main/java/bdbe/bdbd/bay/BayJPARepository.java
+++ src/main/java/bdbe/bdbd/bay/BayJPARepository.java
@@ -4,5 +4,5 @@ import bdbe.bdbd.carwash.Carwash;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BayJPARepository extends JpaRepository<Bay, Long> {
-
+    Bay findFirstBy(); // 하나 찾기
 }
