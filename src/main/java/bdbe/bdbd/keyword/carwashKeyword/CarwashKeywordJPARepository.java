92705138b12534af7fb96d1b7c2013639c439a72
김명지 <audwl03071@gmail.com>
2023-10-09 01:02:26 +0900

    delete:  캐시 삭제

--- src/main/java/bdbe/bdbd/keyword/carwashKeyword/CarwashKeywordJPARepository.java
+++
@@ -1,7 +0,0 @@
-package bdbe.bdbd.keyword.carwashKeyword;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarwashKeywordJPARepository extends JpaRepository<CarwashKeyword, Long> {
-
-}
--- src/main/java/bdbe/bdbd/region/RegionJPARepository.java
+++
@@ -1,7 +0,0 @@
-package bdbe.bdbd.region;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionJPARepository extends JpaRepository<Region, Long> {
-
-}
