f034578669364d64da292127b7163e48f7537cb8
Cheoroo <195822@jnu.ac.kr>
2023-10-13 21:24:23 +0900

    fix: 시큐리티 권한 로직 수정
    주요 변경점은 필터에서 USER_ROLE을 가진 user생성 에서 실제 role값을 반영 하도록 만들었습니다

--- src/main/java/bdbe/bdbd/_core/errors/security/JWTProvider.java
+++ src/main/java/bdbe/bdbd/_core/errors/security/JWTProvider.java
@@ -24,7 +24,7 @@ public class JWTProvider {
                 .withSubject(user.getEmail())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXP))
                 .withClaim("id", user.getId())
-                .withClaim("role", String.valueOf(user.getRole()))
+                .withClaim("role", user.getRole().name())
                 .sign(Algorithm.HMAC512(SECRET));
         return TOKEN_PREFIX + jwt;
     }
