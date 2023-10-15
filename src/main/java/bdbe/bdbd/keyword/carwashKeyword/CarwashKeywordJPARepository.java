1070b51ed9fbbaf69a62e891fc8353b2961bab4f
Cheoroo <195822@jnu.ac.kr>
2023-10-15 18:21:32 +0900

    feat: CarwashKeyword 조회 로직
    키워드들과 연결된 모든 CarwashKeyword 엔티티를 조회

--- src/main/java/bdbe/bdbd/keyword/carwashKeyword/CarwashKeywordJPARepository.java
+++ src/main/java/bdbe/bdbd/keyword/carwashKeyword/CarwashKeywordJPARepository.java
@@ -1,7 +1,11 @@
 package bdbe.bdbd.keyword.carwashKeyword;
 
+import bdbe.bdbd.keyword.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarwashKeywordJPARepository extends JpaRepository<CarwashKeyword, Long> {
+    List<CarwashKeyword> findByKeywordIn(List<Keyword> keywords);
 
 }
