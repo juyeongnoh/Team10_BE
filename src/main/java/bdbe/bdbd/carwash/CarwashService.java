41e7b8dcdef4f17fc6a49ba4999be09661786c06
Cheoroo <195822@jnu.ac.kr>
2023-10-15 18:20:11 +0900

    feat: 주변 세차장 찾는 기능 + 키워드 검색 기능에 대한 서비스 구현

--- src/main/java/bdbe/bdbd/carwash/CarwashService.java
+++ src/main/java/bdbe/bdbd/carwash/CarwashService.java
@@ -1,6 +1,7 @@
 package bdbe.bdbd.carwash;
 
 
+import bdbe.bdbd._core.errors.utils.Haversine;
 import bdbe.bdbd.keyword.Keyword;
 import bdbe.bdbd.keyword.KeywordJPARepository;
 import bdbe.bdbd.keyword.carwashKeyword.CarwashKeyword;
@@ -18,9 +19,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 //@Transactional(readOnly = true)
@@ -84,4 +83,63 @@ public class CarwashService {
         }
         carwashKeywordJPARepository.saveAll(carwashKeywordList);
     } //변경감지, 더티체킹, flush, 트랜잭션 종료
+
+    
+    public List<CarwashRequest.CarwashDistanceDTO> findNearbyCarwashesByUserLocation(CarwashRequest.UserLocationDTO userLocation) {
+        List<Carwash> carwashes = carwashJPARepository.findCarwashesWithin10Kilometers(userLocation.getLatitude(), userLocation.getLongitude());
+
+        return carwashes.stream()
+                .map(carwash -> {
+                    double distance = Haversine.distance(userLocation.getLatitude(), userLocation.getLongitude(),
+                            carwash.getLocation().getLatitude(), carwash.getLocation().getLongitude());
+
+                    CarwashRequest.CarwashDistanceDTO dto = new CarwashRequest.CarwashDistanceDTO(carwash.getId(), carwash.getName(), carwash.getLocation(), distance);
+                    return dto;
+                })
+                .sorted(Comparator.comparingDouble(CarwashRequest.CarwashDistanceDTO::getDistance))
+                .collect(Collectors.toList());
+    }
+
+    public CarwashRequest.CarwashDistanceDTO findNearestCarwashByUserLocation(CarwashRequest.UserLocationDTO userLocation) {
+        List<Carwash> carwashes = carwashJPARepository.findCarwashesWithin10Kilometers(userLocation.getLatitude(), userLocation.getLongitude());
+
+        return carwashes.stream()
+                .map(carwash -> {
+                    double distance = Haversine.distance(userLocation.getLatitude(), userLocation.getLongitude(),
+                            carwash.getLocation().getLatitude(), carwash.getLocation().getLongitude());
+
+                    CarwashRequest.CarwashDistanceDTO dto = new CarwashRequest.CarwashDistanceDTO(carwash.getId(), carwash.getName(), carwash.getLocation(), distance);
+                    return dto;
+                })
+                .min(Comparator.comparingDouble(CarwashRequest.CarwashDistanceDTO::getDistance))
+                .orElse(null);
+    }
+
+    public List<CarwashRequest.CarwashDistanceDTO> findCarwashesByKeywords(CarwashRequest.SearchRequestDTO searchRequest) {
+        List<Carwash> carwashesWithin10Km = carwashJPARepository.findCarwashesWithin10Kilometers(searchRequest.getLatitude(), searchRequest.getLongitude());
+
+        List<Keyword> selectedKeywords = keywordJPARepository.findByType(1).stream()
+                .filter(keyword -> searchRequest.getKeywords().contains(keyword.getName()))
+                .collect(Collectors.toList());
+
+        List<CarwashKeyword> carwashKeywords = carwashKeywordJPARepository.findByKeywordIn(selectedKeywords);
+
+        Set<Long> carwashIdsWithSelectedKeywords = carwashKeywords.stream()
+                .map(carwashKeyword -> carwashKeyword.getCarwash().getId())
+                .collect(Collectors.toSet());
+
+        List<CarwashRequest.CarwashDistanceDTO> result = carwashesWithin10Km.stream()
+                .filter(carwash -> carwashIdsWithSelectedKeywords.contains(carwash.getId()))
+                .map(carwash -> {
+                    double distance = Haversine.distance(
+                            searchRequest.getLatitude(), searchRequest.getLongitude(),
+                            carwash.getLocation().getLatitude(), carwash.getLocation().getLongitude()
+                    );
+                    return new CarwashRequest.CarwashDistanceDTO(carwash.getId(), carwash.getName(), carwash.getLocation(), distance);
+                })
+                .sorted(Comparator.comparingDouble(CarwashRequest.CarwashDistanceDTO::getDistance))
+                .collect(Collectors.toList());
+
+        return result;
+    }
 }
