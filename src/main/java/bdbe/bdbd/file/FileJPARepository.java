76a13e6101d74028cf4e8c235d95683753edbec8
Cheoroo <195822@jnu.ac.kr>
2023-10-22 12:40:53 +0900

    fix: 필요 없어진 findByName 삭제

--- src/main/java/bdbe/bdbd/file/FileJPARepository.java
+++ src/main/java/bdbe/bdbd/file/FileJPARepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FileJPARepository extends JpaRepository<File, Long> {
-    Optional<File> findByName(String name);
-
 }
