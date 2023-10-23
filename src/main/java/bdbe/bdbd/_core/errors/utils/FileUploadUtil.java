49c1f273f998cbcae9aafd47b0a14b6e21fe9061
김명지 <audwl03071@gmail.com>
2023-10-23 16:39:36 +0900

    feat: 여러개의 파일 업로드하는 기능 구현 여러개의 파일이 동시에 업로드되도록 구현

--- src/main/java/bdbe/bdbd/_core/errors/utils/FileUploadUtil.java
+++ src/main/java/bdbe/bdbd/_core/errors/utils/FileUploadUtil.java
@@ -20,6 +20,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FileUploadUtil {
@@ -105,5 +107,53 @@ public class FileUploadUtil {
                 newFile,
                 newFile.getCarwash().getId()
         );
+    }
+
+    /**
+     * 여러 개의 MultipartFile 객체를 처리하여 각각을 Amazon S3에 업로드한 후,
+     * 각 파일에 대한 메타데이터를 데이터베이스에 저장
+     *
+     * @param multipartFiles MultipartFile 객체의 배열, 웹 요청으로부터 받은 업로드 파일들
+     * @param carwashId 업로드할 파일들과 연관된 Carwash 엔터티의 ID
+     * @return List<FileResponse.SimpleFileResponseDTO> 업로드된 파일들의 메타데이터를 담은 DTO 객체의 리스트
+     * @throws Exception 파일 업로드 및 메타데이터 저장 중에 예외가 발생하면 던짐
+     */
+    public List<FileResponse.SimpleFileResponseDTO> uploadFiles(MultipartFile[] multipartFiles, Long carwashId) throws Exception {
+        Carwash carwash = carwashRepository.findById(carwashId)
+                .orElseThrow(() -> new NotFoundError("Carwash not found"));
+
+        List<FileResponse.SimpleFileResponseDTO> fileResponseList = new ArrayList<>();
+
+        for (MultipartFile multipartFile : multipartFiles) {
+            File file = convertMultiPartToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+
+            try {
+                uploadFileToS3Bucket(fileName, file);
+            } catch (SdkClientException e) {
+                logger.error("File upload failed: {}", e.getMessage());
+                throw e;
+            }
+
+            FileRequest.FileDTO fileDTO = new FileRequest.FileDTO();
+            fileDTO.setName(fileName);
+            fileDTO.setUrl(amazonS3.getUrl(bucketName, fileName).toExternalForm());
+            fileDTO.setPath(file.getPath());
+            fileDTO.setUploadedAt(LocalDateTime.now());
+            fileDTO.setCarwash(carwash);
+
+            bdbe.bdbd.file.File newFile = fileDTO.toEntity();
+            newFile = fileRepository.save(newFile);
+
+            file.delete();  // 로컬 파일 삭제
+
+            FileResponse.SimpleFileResponseDTO fileResponse = new FileResponse.SimpleFileResponseDTO(
+                    newFile,
+                    newFile.getCarwash().getId()
+            );
+            fileResponseList.add(fileResponse);
+        }
+
+        return fileResponseList;
     }
 }
