e72f70df0bee75c0e614df8a6a91a3149b359d02
김명지 <audwl03071@gmail.com>
2023-10-23 16:53:29 +0900

    refactor: util 호출을 서비스로 변경 컨트롤러에서 서비스로 util 호출되도록 변경

--- src/main/java/bdbe/bdbd/file/FileService.java
+++ src/main/java/bdbe/bdbd/file/FileService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Transactional
 @Service
 public class FileService {
@@ -17,6 +19,10 @@ public class FileService {
 
     public FileResponse.SimpleFileResponseDTO uploadFile(MultipartFile multipartFile, Long carwashId) throws Exception {
         return fileUploadUtil.uploadFile(multipartFile, carwashId);
+    }
+
+    public List<FileResponse.SimpleFileResponseDTO> uploadFiles(MultipartFile[] multipartFile, Long carwashId) throws Exception {
+        return fileUploadUtil.uploadFiles(multipartFile, carwashId);
     }
 
 }
