1bb520b42e054788ffa4e47e84d7354d8cd13767
김명지 <audwl03071@gmail.com>
2023-10-23 16:53:29 +0900

    refactor: util 호출을 서비스로 변경 컨트롤러에서 서비스로 util 호출되도록 변경

--- src/main/java/bdbe/bdbd/file/FileRestController.java
+++ src/main/java/bdbe/bdbd/file/FileRestController.java
@@ -1,6 +1,5 @@
 package bdbe.bdbd.file;
 
-import bdbe.bdbd._core.errors.utils.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +16,12 @@ import java.util.List;
 @RequestMapping("/api/files")
 public class FileRestController {
 
-//    private final FileService fileService;
+    private final FileService fileService;
 
     @Autowired
-    private FileUploadUtil fileUploadUtil;
-
-
-//    @Autowired
-//    public FileRestController(FileService fileService) {
-//        this.fileService = fileService;
-//    }
+    public FileRestController(FileService fileService) {
+        this.fileService = fileService;
+    }
 
     @PostMapping("/upload")
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file,
@@ -34,19 +29,19 @@ public class FileRestController {
         if (file.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid file");
         }
-        FileResponse.SimpleFileResponseDTO response = fileUploadUtil.uploadFile(file, carwashId);
+        FileResponse.SimpleFileResponseDTO response = fileService.uploadFile(file, carwashId);
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/uploadMultiple")
-    public ResponseEntity<?> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files,
-                                                 @RequestParam("carwashId") Long carwashId) throws Exception {
+    @PostMapping("/uploadMultipleFile")
+    public ResponseEntity<?> uploadFiles(@RequestParam("files") MultipartFile[] files,
+                                         @RequestParam("carwashId") Long carwashId) throws Exception {
         if (files == null || files.length == 0) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No files provided");
         }
-
-        List<FileResponse.SimpleFileResponseDTO> responses = fileUploadUtil.uploadFiles(files, carwashId);
-        return ResponseEntity.ok(responses);
+        List<FileResponse.SimpleFileResponseDTO> response = fileService.uploadFiles(files, carwashId);
+        return ResponseEntity.ok(response);
     }
+
 
 }
