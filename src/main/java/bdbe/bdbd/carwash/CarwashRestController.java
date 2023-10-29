82c3b4c365eb2db74f061456760b7e00a072b165
김명지 <audwl03071@gmail.com>
2023-10-29 14:39:23 +0900

    refactor: api 경로에 /api 추가

--- src/main/java/bdbe/bdbd/carwash/CarwashRestController.java
+++ src/main/java/bdbe/bdbd/carwash/CarwashRestController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class CarwashRestController {
 
     private final CarwashService carwashService;
--- src/test/java/bdbe/bdbd/review/ReviewRestControllerTest.java
+++ src/test/java/bdbe/bdbd/review/ReviewRestControllerTest.java
@@ -109,7 +109,7 @@ public class ReviewRestControllerTest {
 
         // when
         ResultActions resultActions = mvc.perform(
-                post("/reviews")
+                post("/api/reviews")
                         .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
         );
@@ -217,7 +217,7 @@ public class ReviewRestControllerTest {
 
         // when
         ResultActions resultActions = mvc.perform(
-                MockMvcRequestBuilders.get(String.format("/carwashes/%d/reviews", carwashId))
+                MockMvcRequestBuilders.get(String.format("/api/carwashes/%d/reviews", carwashId))
         );
 
         // eye
@@ -247,7 +247,7 @@ public class ReviewRestControllerTest {
 
         // when
         ResultActions resultActions = mvc.perform(
-                MockMvcRequestBuilders.get("/reviews")
+                MockMvcRequestBuilders.get("/api/reviews")
         );
 
         // eye
